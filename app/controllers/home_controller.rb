cddeaf00fd6fc15bc54e48bbf0c2da9065f225ab
Samantha Tsang <35239591+smytsang@users.noreply.github.com>
2018-03-17 14:07:16 -0700

    Views (#7)
    * Add sessions route and new session view
    
    * Create home#index page
    
    * Update home page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    @hide_navbar = true
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :password_resets, only: [:edit, :update]
 
   resources :attempts
+  resource :session, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :drill_groups do
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get('/', { to: 'home#index', as: :root })
   get('/sign_up/thanks', { to: 'users#thanks' })
   get('/forgot_password', {to: 'password_resets#forgot_password'})
 
